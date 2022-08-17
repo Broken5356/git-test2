0725a4bfdda217b64d5f6b6c06252fe2f5550e75
Broken <Broken@wind.com>
2022-08-17 19:19:47 +0800

    master  commit2

--- src/main/java/org/broken/git/GitDemo.java
+++ src/main/java/org/broken/git/GitDemo.java
@@ -6,5 +6,6 @@ public class GitDemo {
         System.out.println("Hello Git!2");
         System.out.println("Hello Git!3");
         System.out.println("Hello Git!4");
+        System.out.println("master test");
     }
 }
